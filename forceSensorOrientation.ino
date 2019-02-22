983139600ffde97c71a906fd2e0fc9c9ab36b701
ehn3@njit.edu <ehn3@njit.edu>
2019-02-22 15:51:08 -0500

    Fixed variable misnaming

--- forceSensorOrientation.ino
+++ forceSensorOrientation.ino
@@ -7,7 +7,7 @@
 
 void sensorOrientation(){
   presElbowAng     = (presPosElbow - ELBOW_MIN_POS)    * DEGREES_PER_COUNT * (PI/180);
-  presShoulderAng  = (presPosShoulder - SHOULDER_MIN_POS) * DEGREES_PER_COUNT * (PI/180);
+  presShoulderAng  = (presPosShoulder) * DEGREES_PER_COUNT * (PI/180);
   Fx = FxRaw * (cos(presShoulderAng + presElbowAng)) + FyRaw * (-sin(presShoulderAng + presElbowAng));
   Fy = FxRaw * (sin(presShoulderAng + presElbowAng)) + FyRaw * ( cos(presShoulderAng + presElbowAng));
 }
