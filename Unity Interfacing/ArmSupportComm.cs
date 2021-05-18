38c08d89746705f9c5d83543d31de055908bf2d5
ehn3@njit.edu <ehn3@njit.edu>
2021-05-18 16:10:30 -0400

    Unity Interfacing

---
+++ Unity Interfacing/ArmSupportComm.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArmSupportComm : MonoBehaviour
+{
+    public string comPort;
+    public int baudRate;
+
+    private int rxPacketLen;
+
+    private float dt;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
