6dbb1c34c29ed1aeb30a00245e2f3413e48be193
erick <ercknz@gmail.com>
2021-05-24 17:59:02 -0400

    Working on unity interfacing

--- Unity Interfacing/ArmSupportComm.cs
+++ Unity Interfacing/ArmSupportComm.cs
@@ -14,38 +14,74 @@ public class ArmSupportComm : MonoBehaviour
     private byte[] rxHeader     = {170, 8, 69, 0};
     private byte[] modHeader    = {150, 10, 10, 96};
     private byte[] configHeader = {150, 0, 69, 8};
+    private byte[] rxBuffer;
 
     private float dt = 0.008f;
 
-    float X;
-    float Y;
-    float Z;
-    float U;
-    float V;
-    float W;
+    private float X;
+    private float Y;
+    private float Z;
+    private float U;
+    private float V;
+    private float W;
+
+    private bool initialPositionSet = false;
+    private float initialX;
+    private float initialY;
+    private float initialZ;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ArmSupportComm = new SerialPort();
+        ArmSupportComm.PortName = comPort;
+        ArmSupportComm.BaudRate = baudRate;
+        ArmSupportComm.ReadTimeout = 500;
+        ArmSupportComm.Open();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private SerialPort InitializeArmSupportComm(){
-
+        ReadArmSupportComm();
     }
 
     private void ReadArmSupportComm(){
-
+        while (ByteToRead < rxPacketLen ){}
+        ArmSupportComm.Read(rxBuffer, 0, rxPacketLen);
+        if (rxBuffer[0] == rxHeader[0] && rxBuffer[1] == rxHeader[1] && rxBuffer[2] == rxHeader[2] && rxBuffer[3] == rxHeader[3]){
+            ushort cSum = 0;
+            for (int i = 0; i < rxPacketLen - 2; i++){
+                cSum += rxBuffer[i];
+            }
+            ushort packetCS = ByteArrayToUint16(rxBuffer[rxPacketLen-2], rxBuffer[rxPacketLen-1]);
+            if (cSum == packetCS){
+                // End effector positional data
+                float xVal = ByteArrayToFloat(rxBuffer[20], rxBuffer[21], rxBuffer[22], rxBuffer[23]);
+                float yVal = ByteArrayToFloat(rxBuffer[24], rxBuffer[25], rxBuffer[26], rxBuffer[27]);
+                float zVal = ByteArrayToFloat(rxBuffer[28], rxBuffer[29], rxBuffer[30], rxBuffer[31]);
+                SetPos(xVal, yVal, zVal);
+            }
+        }
     }
 
-    private void SetPos(){
+    private void SetPosition(float xPos, float yPos, float zPos){
+        if (!initialPositionSet){
+            initialPositionSet = true;
+            initialX = xPos;
+            initialY = yPos;
+            initialZ = zPos;
+        } else {
+            X = initialX - xPos;
+            Y = initialY - yPos;
+            Z = initialZ - zPos;
+        }
+    }
 
+    public void GetArmSupportPosition(out xValue, out yValue, out zValue){
+        xValue = X;
+        yValue = Y;
+        zValue = Z;
     }
 
     private void SetVel(){
