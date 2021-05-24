ba38ddb491b5f05eeb5f03db8bddb6a60b4ae100
erick <ercknz@gmail.com>
2021-05-23 22:04:16 -0400

    working on unity interfacing

--- Unity Interfacing/ArmSupportComm.cs
+++ Unity Interfacing/ArmSupportComm.cs
@@ -7,16 +7,22 @@ public class ArmSupportComm : MonoBehaviour
 {
     public string comPort = "Com24";
     public int baudRate = 115200;
+    private SerialPort ArmSupportSerialPort;
 
     const private int rxPacketLen = 98;
-    const private int txPacketLen = 35;
-    private byte[] rxHeader = {};
-    private byte[] configHeader = {};
-    private byte[] modHeader = {};
-
+    const private int txPacketLen = 39;
+    private byte[] rxHeader     = {170, 8, 69, 0};
+    private byte[] modHeader    = {150, 10, 10, 96};
+    private byte[] configHeader = {150, 0, 69, 8};
 
     private float dt = 0.008f;
 
+    float X;
+    float Y;
+    float Z;
+    float U;
+    float V;
+    float W;
 
     // Start is called before the first frame update
     void Start()
@@ -30,12 +36,21 @@ public class ArmSupportComm : MonoBehaviour
         
     }
 
-    short ByteArrayToInt16(){
+    private void SetPos(){
 
     }
 
-    float ByteArrayToFloat(){
+    private void SetVel(){
 
+    }
+
+    private ushort ByteArrayToUint16(byte hiByte, byte loByte){
+        return hiByte * 256 + loByte;
+    }
+
+    private float ByteArrayToFloat(byte byte1, byte byte2, byte byte3, byte byte4){
+        int32_t inInt = ((byte4 & 0xFF) << 24 | (byte3 & 0xFF) << 16 | (byte2 & 0xFF) << 8 | (byte1 & 0xFF));
+        return (float)(inInt / 10000.0f);
     }
 
 }
