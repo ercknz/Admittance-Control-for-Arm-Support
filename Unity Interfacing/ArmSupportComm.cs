6cabc932329f5407dc03b2a2d60abe3779958419
erick <ercknz@gmail.com>
2021-05-27 16:16:45 -0400

    Working on unity features

--- Unity Interfacing/ArmSupportComm.cs
+++ Unity Interfacing/ArmSupportComm.cs
@@ -24,20 +24,16 @@ public class ArmSupportComm : MonoBehaviour
 
     //private const float dt = 0.008f;
 
-    private Vector3 GlobalForceVector;
-    private Vector3 PositionVector;
-    private Vector3 InitialPositionVector;
-    private Vector3 VelocityVector;
-    JointSpace presQ = new JointSpace(0.0f, 0.0f, 0.0f);
+    private Vector3 GlobalForceVector = new Vector3;
+    private Vector3 PositionVector = new Vector3;
+    private Vector3 InitialPositionVector = new Vector3;
+    private Vector3 VelocityVector = new Vector3;
+    JointSpace presQ = new JointSpace;
+    
     struct JointSpace{
         public float Q1;
         public float Q2;
         public float Q4;
-        public JointSpace(float q1, float q2, float q4){
-            this.Q1 = q1;
-            this.Q2 = q2;
-            this.Q4 = q4;
-        }
     }
 
     private bool initialPositionSet = false;
@@ -94,11 +90,15 @@ public class ArmSupportComm : MonoBehaviour
                     }
                     ushort packetCS = ByteArrayToUint16(rxBuffer[rxPacketLen - 2], rxBuffer[rxPacketLen - 1]);
                     if (cSum == packetCS){
+                        // Global Forces (note: Unity.Y = Robot.Z)
+
                         // End effector Position Data (note: Unity.Y = Robot.Z)
                         float xVal = ByteArrayToFloat(rxBuffer[20], rxBuffer[21], rxBuffer[22], rxBuffer[23]);
                         float yVal = ByteArrayToFloat(rxBuffer[28], rxBuffer[29], rxBuffer[30], rxBuffer[31]);
                         float zVal = ByteArrayToFloat(rxBuffer[24], rxBuffer[25], rxBuffer[26], rxBuffer[27]);
                         SetPosition(xVal, yVal, zVal);
+
+                        // End Effector Velocity Data (Note: Unity.Y = Robot.Z)
 
                         // JointSpace of robot
                         presQ.Q1 = ByteArrayToFloat(rxBuffer[44], rxBuffer[45], rxBuffer[46], rxBuffer[47]);
@@ -112,11 +112,8 @@ public class ArmSupportComm : MonoBehaviour
         }
     }
 
-    private void SetPosition(float x, float y, float z){
-        // Fix Rotation Offset
-        float xPos = (float)(x * Math.Cos(frameOffsetAngle) - z * Math.Sin(frameOffsetAngle));
-        float yPos = y;
-        float zPos = (float)(x * Math.Sin(frameOffsetAngle) + z * Math.Cos(frameOffsetAngle));
+    private void SetPosition(float xPos, float yPos, float zPos){
+        FixRotationalOffset(ref xPos, ref zPos)
         // Debug.Log("Packet -> X: " + xPos + " Y: " + yPos + " Z: " + zPos);
         if (!initialPositionSet){
             initialPositionSet = true;
@@ -126,8 +123,9 @@ public class ArmSupportComm : MonoBehaviour
         }
     }
 
-    private void SetVel(){
-
+    private void SetVel(float xVel, float yVel, float zVel){
+        FixRotationalOffset(ref xVel, ref zVel)
+        VelocityVector = new Vector3(xVel, yVel, zVel);
     }
 
     private ushort ByteArrayToUint16(byte hiByte, byte loByte){
@@ -138,5 +136,12 @@ public class ArmSupportComm : MonoBehaviour
     private float ByteArrayToFloat(byte byte1, byte byte2, byte byte3, byte byte4){
         int inInt = ((byte4 & 0xFF) << 24 | (byte3 & 0xFF) << 16 | (byte2 & 0xFF) << 8 | (byte1 & 0xFF));
         return (float)(inInt / 10000.0f);
+    }
+
+    private void FixRotationalOffset(ref float x, ref float z){
+        float tempX = (float)(x * Math.Cos(frameOffsetAngle) - z * Math.Sin(frameOffsetAngle));
+        float tempZ = (float)(x * Math.Sin(frameOffsetAngle) + z * Math.Cos(frameOffsetAngle));
+        x = tempX;
+        z = tempZ;
     }
 }
